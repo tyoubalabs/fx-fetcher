fa005bcbc066ab81752d7075946c20463e5483cd
DorraZr <2254250@bded.qc.ca>
2025-12-05 00:24:46 -0500

    remove EGP

--- main.py
+++ main.py
@@ -41,10 +41,7 @@ async def fetch_moneygram_rate(results):
 
 # Western Union configuration mapping
 WU_CONFIG = {
-    ("CAD", "EGP"): {
-        "url": "https://www.westernunion.com/ca/en/send-money-to-egypt.html",
-        "selector": 'xpath=//*[@id="body-component"]/section[1]/section[1]/div[1]/div/div/div[2]/p/span[1]/span[1]/span/span'
-    },
+
     ("CAD", "TND"): {
         "url": "https://www.westernunion.com/ca/en/send-money-to-tunisia.html",
         "selector": 'xpath=//*[@id="body-component"]/section[1]/section[1]/div[1]/div/div/div[2]/p/span[1]/span[1]/span/span'
@@ -53,10 +50,7 @@ WU_CONFIG = {
         "url": "https://www.westernunion.com/ca/en/send-money-to-morocco.html",
         "selector": 'xpath=//*[@id="body-component"]/section[1]/section[1]/div[1]/div/div/div[2]/p/span[1]/span[1]/span/span'
     },
-    ("USD", "EGP"): {
-        "url": "https://www.westernunion.com/us/en/web/send-money/start?ReceiveCountry=EG&ISOCurrency=EGP&SendAmount=100.00&FundsOut=BA&FundsIn=WUPay.html",
-        "selector": 'xpath=//*[@id="smoExchangeRate"]/text()[2]'
-    },
+
     ("USD", "TND"): {
         "url": "https://www.westernunion.com/us/en/currency-converter/usd-to-tnd-rate.html",
         "selector": 'xpath=//*[@id="body-component"]/section[1]/section[1]/div[1]/div/div/div[3]/p/span[1]/span[1]/span/span'  # adjust after inspecting
@@ -73,10 +67,7 @@ WU_CONFIG = {
         "url": "https://www.westernunion.com/fr/en/send-money-to-morocco.html",
         "selector": 'xpath=//*[@id="body-component"]/section[1]/section[1]/div[1]/div/div/div[2]/p/span[1]/span[1]/span/span'
     },
-    ("EUR", "EGP"): {
-        "url": "https://www.westernunion.com/de/en/web/send-money/start?ReceiveCountry=EG&ISOCurrency=EGP&SendAmount=100.00&FundsOut=BA&FundsIn=WUPay.html",
-        "selector": 'xpath=//*[@id="smoExchangeRate"]/text()[2]'
-    },
+    
 }
 
 
@@ -95,12 +86,11 @@ async def fetch_wu_rate(results, from_currency: str, to_currency: str):
                 args=["--disable-blink-features=AutomationControlled"]
             )
             page = await browser.new_page()
-            await page.goto(config["url"], timeout=60000)
-            html = await page.content()
-            print(html[:2000])
-            await page.wait_for_selector(config["selector"], timeout=60000)
-            text = await page.locator(config["selector"]).inner_text()
+            await page.goto(config["url"], wait_until="domcontentloaded")
 
+            locator = page.locator(config["selector"])
+            await locator.wait_for()
+            text = await locator.inner_text()
             print(f"[WU DEBUG] Captured text for {from_currency}->{to_currency}: {text}")
 
             match = re.search(r"([\d.]+)", text)
